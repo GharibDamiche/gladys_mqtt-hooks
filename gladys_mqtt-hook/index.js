60eb594766c4dc3bf3234312ff6b11f8174d08a2
GharibDamiche <38864906+GharibDamiche@users.noreply.github.com>
2018-04-30 20:27:34 +0200

    update

--- gladys_mqtt-hook/index.js
+++ gladys_mqtt-hook/index.js
@@ -3,6 +3,6 @@ module.exports = function (sails) {
     var exec = require('./lib/exec.js');
     //var connect = require('./lib/connect.js');
     return {
-        exec,
+        exec: exec
     };
 };
