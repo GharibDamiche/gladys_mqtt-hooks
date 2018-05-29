31b0edc68641f7c6c9d0e673e0c76e01f73190a6
GharibDamiche <38864906+GharibDamiche@users.noreply.github.com>
2018-05-29 02:22:11 +0200

    m

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = function (sails) {
 
     var exec = require('./lib/exec.js');
-    //var connect = require('./lib/connect.js');
+    var connect = require('./lib/connect.js');
+
+    gladys.on('ready', function(){
+      connect();
+    });
     return {
         exec: exec
     };
